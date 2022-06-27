4b05bfdd6c74ecdb387159e224cae2d3c5308c5a
Samet <87937886+sametgundogan@users.noreply.github.com>
2022-06-27 15:39:19 +0300

    Update E-Commerce Project.sql

--- E-Commerce Project.sql
+++ E-Commerce Project.sql
@@ -4,7 +4,7 @@ set order_date = concat(SUBSTRING(order_date,7,4),'-' , SUBSTRING(order_date,4,2
 where order_date like '[0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]'
 
 alter table orders_dimen
-ALTER COLUMN order_date date; -- text ten nvarchar a ondan date e çevirdim
+ALTER COLUMN order_date date; 
 
 update shipping_dimen
 set ship_date = concat(SUBSTRING(ship_date,7,4),'-' , SUBSTRING(ship_date,4,2),'-', SUBSTRING(ship_date, 1,2))
@@ -66,7 +66,7 @@ alter TABLE market_fact
 ALTER COLUMN ord_id int
 
 --1. Using the columns of “market_fact”, “cust_dimen”, “orders_dimen”, “prod_dimen”, “shipping_dimen”,
--- Create a new table, named as “combined_table”.
+-- I created a new table, named as “combined_table”.
 SELECT * INTO combined_table
 FROM (
 SELECT mf.Ord_id, mf.Prod_id, mf.Ship_id, mf.Cust_id, mf.Sales, mf.Discount, mf.Order_Quantity, mf.Product_Base_Margin,
@@ -85,20 +85,20 @@ JOIN prod_dimen pd
 on pd.Prod_id = mf.Prod_id
 ) as a
 
---2. Find the top 3 customers who have the maximum count of orders.
+--2. To find the top 3 customers who have the maximum count of orders:
 
 select top 3  Cust_id, customer_name, count(distinct Ord_id) count_of_orders
 from combined_table
 GROUP by Cust_id, Customer_Name
 ORDER by 3 desc
 
--- 3. Create a new column at combined_table as DaysTakenForDelivery that contains the date difference of Order_Date and Ship_Date.
+-- 3. To create a new column at combined_table as DaysTakenForDelivery that contains the date difference of Order_Date and Ship_Date:
 
 select *, DATEDIFF(DAY, Order_Date,ship_date) DaysTakenForDelivery
 from combined_table
 
 
--- 4. Find the customer whose order took the maximum time to get delivered.
+-- 4. To find the customer whose order took the maximum time to get delivered:
 
 SELECT *
 FROM  combined_table ct, (
@@ -107,7 +107,7 @@ FROM  combined_table ct, (
 ) as df
 where DATEDIFF(DAY, Order_Date,ship_date) = DaysTakenForDelivery
  
--- 5. Count the total number of unique customers in January and how many of them came back every month over the entire year in 2011
+-- 5. To count the total number of unique customers in January and how many of them came back every month over the entire year in 2011:
 
 select  COUNT( distinct Cust_id) 
 from combined_table
@@ -128,11 +128,11 @@ PIVOT (
         for ORDER_month 
         in ([1],[2],[3],[4],[5],[6],[7],[8],[9],[10],[11],[12])
 ) as pvt_t -- up to here, the code returns monthly information about purchasing for customers who bought in January
-where [1] >0 and [2] > 0 and [3] > 0 and [4] > 0 -- but the question asks to return the customer who purchased in all months
--- and since there's no customer who already purchased first 4 months, I didn't questioned for the rest
+where [1] >0 and [2] > 0 and [3] > 0 and [4] > 0 -- but to return the customer who purchased in all months
+-- and since there's no customer who already purchased first 4 months, I didn't questioned for the rest.
 
--- 6. Write a query to return for each user the time elapsed between the first purchasing and the third purchasing, 
--- in ascending order by Customer ID.
+-- 6. To return for each user the time elapsed between the first purchasing and the third purchasing, 
+-- in ascending order by Customer ID:
 
 with cte as (
         SELECT cust_id, Order_Date, row_number() OVER(PARTITION by cust_id order by order_date) order_time,
@@ -150,8 +150,8 @@ from cte
 where order_time = 1
 order by Cust_id
 
--- 7. Write a query that returns customers who purchased both product 11 and product 14, as well as the ratio of these 
--- products to the total number of products purchased by the customer.
+-- 7. To return customers who purchased both product 11 and product 14, as well as the ratio of these 
+-- products to the total number of products purchased by the customer:
 
 with cte as(
         select cust_id, prod_id, Order_Quantity, sum(Order_Quantity) OVER (partition by cust_id) summ
@@ -172,13 +172,13 @@ where prod_id in (11,14)
 
 
 -----------------O-O-O--------------------
--- 1. Create a “view” that keeps visit logs of customers on a monthly basis. (For each log, three field is kept: Cust_id, Year, Month)
+-- 1. To create a “view” that keeps visit logs of customers on a monthly basis:
 
 CREATE VIEW  visit_logs as
 select distinct Cust_id,year(order_date) year_, month(Order_Date) month_
 from combined_table
 
--- 2. Create a “view” that keeps the number of monthly visits by users. (Show separately all months from the beginning business)
+-- 2. To create a “view” that keeps the number of monthly visits by users:
 
 create or alter view quant_logs as
 select *
@@ -193,19 +193,19 @@ PIVOT(
 ) pivt
 
 
--- 3. For each visit of customers, create the next month of the visit as a separate column. 
+-- 3. To create the next month of the visit as a separate column, for each visit of customers: 
 
 select *, lead(month_,1) over(partition by  cust_id, year_ order by cust_id, year_,month_) next_month_visit
 from visit_logs
 order by 1,2
 
--- 4. Calculate the monthly time gap between two consecutive visits by each customer.
+-- 4. To calculate the monthly time gap between two consecutive visits by each customer:
 
 select *, lead(month_,1) over(partition by  cust_id, year_ order by cust_id, year_,month_) - month_ monthly_gap
 from visit_logs
 order by 1,2
 
--- 5. Categorise customers using average time gaps. Choose the most fitted labeling model for you.
+-- 5. To categorise customers using average time gaps. Choose the most fitted labeling model for you:
 
 
 -- To solve this question, as there's no customer who purchased in every month, I thought a person counts as 'regular' 
@@ -224,7 +224,7 @@ select cust_id, year_,month_,case
         END aver_type
 from  cte
 
--- 1. Find the number of customers retained month-wise. (You can use time gaps)
+-- 1. To find the number of customers retained month-wise:
 
 create or alter view time_gap as(
         select *, lead(month_,1) over(partition by  cust_id, year_ order by cust_id, year_,month_) - month_ monthly_gap
@@ -235,7 +235,7 @@ from time_gap
 where monthly_gap = 1
 order by 2, 3 -- 381 rows return, that means 381 customers have retained...
 
--- 2. Calculate the month-wise retention rate.
+-- 2. To calculate the month-wise retention rate:
 
 create or alter view TotalCMonthly as(
         select distinct cust_id, year_,month_ ,count(cust_id) OVER(PARTITION by year_,month_) as TotalCustomersintheMonth -- Shows the month has how many customers.
